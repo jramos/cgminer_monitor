311dc0d0622e553ad72eefb045ee494661716fc4
Justin Ramos <justin.ramos@gmail.com>
2014-08-10 19:08:38 -0500

    prelim cli

--- lib/cgminer_monitor/logger.rb
+++ lib/cgminer_monitor/logger.rb
@@ -8,6 +8,7 @@ module CgminerMonitor
 
     def log!
       created_at = Time.now
+      documents = []
 
       CgminerMonitor::Document.document_types.each do |klass|
         if query_results = @miner_pool.query(klass.to_s.demodulize.downcase)
@@ -15,8 +16,11 @@ module CgminerMonitor
           doc.update_attribute(:results, query_results)
           doc.update_attribute(:created_at, created_at)
           doc.save!
+          documents << doc
         end
       end
+
+      documents
     end
   end
 end
