f03930e89661d63f91ce0f8d26030cfd12463fdb
Justin Ramos <justin.ramos@gmail.com>
2014-08-11 10:06:41 -0500

    local_hashrate graph data

--- app/controllers/cgminer_monitor/api/v1/graph_data_controller.rb
+++ app/controllers/cgminer_monitor/api/v1/graph_data_controller.rb
@@ -2,20 +2,41 @@ module CgminerMonitor
   module Api
     module V1
       class GraphDataController < ActionController::Base
-        def devs
-          render :json => []
+        def local_hashrate
+          summaries = CgminerMonitor::Document::Summary.where(:created_at.gt => created_at_from_range)
+
+          response = summaries.collect do |summary|
+            [
+              summary[:created_at].to_i,
+              summary[:results].collect do |miner_result|
+                [
+                  miner_result.first[:ghs_5s],
+                  miner_result.first[:ghs_av],
+                ]
+              end
+            ]
+          end
+
+          render :json => response.as_json
         end
 
-        def pools
-          render :json => []
-        end
+        private
 
-        def stats
-          render :json => []
-        end
-
-        def summary
-          render :json => []
+        def created_at_from_range
+          case params[:range]
+            when 'last_hour'
+              Time.now - 1.hour
+            when 'last_day'
+              Time.now - 1.day
+            when 'last_week'
+              Time.now - 1.week
+            when 'last_month'
+              Time.now - 1.month
+            when 'last_year'
+              Time.now - 1.year
+            else
+              Time.now - 1.hour
+          end
         end
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ CgminerMonitor::Engine.routes.draw do
   namespace :cgminer_monitor do
     namespace :api do
       namespace :v1 do
-        match 'graph_data/devs',    to: 'graph_data#devs',    via: [:get]
-        match 'graph_data/pools',   to: 'graph_data#pools',   via: [:get]
-        match 'graph_data/stats',   to: 'graph_data#stats',   via: [:get]
-        match 'graph_data/summary', to: 'graph_data#summary', via: [:get]
+        namespace :graph_data do
+          get 'local_hashrate'
+        end
       end
     end
   end
