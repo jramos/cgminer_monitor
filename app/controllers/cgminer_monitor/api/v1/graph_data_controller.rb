76ee81b2d734c056f6327514a7f77cb12e394352
Justin Ramos <justin.ramos@gmail.com>
2014-08-11 09:12:00 -0500

    route stubs

--- app/controllers/cgminer_monitor/api/v1/graph_data_controller.rb
+++ app/controllers/cgminer_monitor/api/v1/graph_data_controller.rb
@@ -2,6 +2,9 @@ module CgminerMonitor
   module Api
     module V1
       class GraphDataController < ActionController::Base
+        def index
+          render :json => []
+        end
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 CgminerMonitor::Engine.routes.draw do
+  namespace :cgminer_monitor do
+    namespace :api do
+      namespace :v1 do
+        match 'graph_data', to: 'graph_data#index', via: [:get]
+      end
+    end
+  end
 end
