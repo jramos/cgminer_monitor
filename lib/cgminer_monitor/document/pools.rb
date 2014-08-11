bf2de299dda869f0325887d5409799251bf1affe
Justin Ramos <justin.ramos@gmail.com>
2014-08-11 07:48:27 -0500

    indexes

--- lib/cgminer_monitor/document/pools.rb
+++ lib/cgminer_monitor/document/pools.rb
@@ -2,6 +2,8 @@ module CgminerMonitor
   module Document
     class Pools
       include Mongoid::Document
+
+      index({ created_at: 1 }, { unique: true })
     end
   end
 end
--- lib/cgminer_monitor/document/summary.rb
+++ lib/cgminer_monitor/document/summary.rb
@@ -2,6 +2,8 @@ module CgminerMonitor
   module Document
     class Summary
       include Mongoid::Document
+
+      index({ created_at: 1 }, { unique: true })
     end
   end
 end
