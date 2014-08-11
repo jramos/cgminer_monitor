2b02ae9f507758b78734bc56d7763985a827eb2a
Justin Ramos <justin.ramos@gmail.com>
2014-08-11 09:22:23 -0500

    splitting into various endpoints

--- app/controllers/cgminer_monitor/api/v1/graph_data_controller.rb
+++ app/controllers/cgminer_monitor/api/v1/graph_data_controller.rb
@@ -2,7 +2,19 @@ module CgminerMonitor
   module Api
     module V1
       class GraphDataController < ActionController::Base
-        def index
+        def devs
+          render :json => []
+        end
+
+        def pools
+          render :json => []
+        end
+
+        def stats
+          render :json => []
+        end
+
+        def summary
           render :json => []
         end
       end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ CgminerMonitor::Engine.routes.draw do
   namespace :cgminer_monitor do
     namespace :api do
       namespace :v1 do
-        match 'graph_data', to: 'graph_data#index', via: [:get]
+        match 'graph_data/devs',    to: 'graph_data#devs',    via: [:get]
+        match 'graph_data/pools',   to: 'graph_data#pools',   via: [:get]
+        match 'graph_data/stats',   to: 'graph_data#stats',   via: [:get]
+        match 'graph_data/summary', to: 'graph_data#summary', via: [:get]
       end
     end
   end
